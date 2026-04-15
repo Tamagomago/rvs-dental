f82f9b2916075fd670d28aea789d6194b87e77dc
Jul Leo Javellana <juljavellana1218@gmail.com>
2026-04-15 09:00:31 +0800

    chore: re-implement add patient button & remove teeth chart

--- resources/views/pages/patients/partials/teeth-chart.blade.php
+++
@@ -1,9 +0,0 @@
-<div id="tab-teeth-chart" class="tab-panel hidden">
-    <div class="w-full bg-secondary p-5 rounded-t-xl border-b border-border flex justify-between items-center">
-        <p class="font-bold text-2xl md:text-3xl">Teeth Chart</p>
-        <div class="text-xs flex flex-col items-end text-right">
-            <p>LAST UPDATED</p>
-            <p>{date}</p>
-        </div>
-    </div>
-</div>
